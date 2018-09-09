639eb9f3cadd6d8a472d1817234ac7fd288d8494
League <League@adminisorsimac2.attlocal.net>
2018-09-09 11:27:32 -0700

    tweet

---
+++ src/LatestTweet.java
@@ -0,0 +1,64 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+public class LatestTweet implements ActionListener{
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JTextField field = new JTextField(15);
+JButton button = new JButton();
+public void globglogabgalab() {
+	frame.add(panel);
+	frame.setVisible(true);
+	frame.setSize(50,10);
+	frame.setTitle("LatestTweet");
+	panel.add(field);
+	panel.add(button);	
+	button.setText("Search the Twitterverse");
+	frame.pack();
+	button.addActionListener(this);
+}
+public static void main(String[] args) {
+	LatestTweet tweet = new LatestTweet();
+
+	tweet.globglogabgalab();
+}
+public void actionPerformed(ActionEvent event) {
+	JButton buttonPressed = (JButton) event.getSource(); 
+	if(buttonPressed.equals(button)) {
+		System.out.println("Tweet Tweet");
+		tweet.getLatestTweet(field);
+
+	}
+	}
+private String getLatestTweet(String searchingFor) {
+
+    Twitter twitter = new TwitterFactory().getInstance();
+
+    AccessToken accessToken = new AccessToken(
+          "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+          "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+
+    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+          "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+
+    twitter.setOAuthAccessToken(accessToken);
+
+    Query query = new Query(searchingFor);
+    try {
+          QueryResult result = twitter.search(query);
+          return result.getTweets().get(0).getText();
+    } catch (Exception e) {
+          System.err.print(e.getMessage());
+          return "What the heck is that?";
+    }
+}
+}
