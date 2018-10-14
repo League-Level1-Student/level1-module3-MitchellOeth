3d48835dee1b96f88991c6f5079b7eb3052613aa
league <league@imac-9.attlocal.net>
2018-10-14 11:11:35 -0700

    whack

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,23 +1,31 @@
 import java.util.Random;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 public class WhackAMole {
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
-static WhackAMole whack = new WhackAMole();
 public static void main(String[] args) {
-	whack.globglogabgalab();
+	 WhackAMole whack = new WhackAMole();
+whack.globglogabgalab();
+	String jeff = JOptionPane.showInputDialog("How many buttons do you want to create?");
+	int joe = Integer.parseInt(jeff); 
+	whack.drawButtons(joe);
 }
 private void globglogabgalab() {
-frame.add(panel);
+
 frame.setVisible(true);
-frame.setSize(100,100);
+frame.setSize(2040,1900);
 }
 public void drawButtons(int bob) {
+	Random rand = new Random();
 	for (int i = 0; i < bob; i++) {
-		frame.add(new JButton());
+		panel.add(new JButton());
+		if (i==rand.nextInt()) {
+			JButton NoThisIsPatrick = new JButton("me!");
+		}
 	}
+	frame.add(panel);
 }
 }
