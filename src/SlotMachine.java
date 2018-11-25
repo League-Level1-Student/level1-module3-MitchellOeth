52d13858bb38127eaba38a3ee521b3b822e6ccf9
Dong Zou <jiwenzou@gmail.com>
2018-11-25 11:32:17 -0800

    slot

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,3 +1,6 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.Random;
 import javax.swing.Icon;
@@ -5,71 +8,112 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-public class SlotMachine {
-Random rand = new Random();
-JButton button = new JButton();
-JLabel left = new JLabel();
-JLabel center = new JLabel();
-JLabel right = new JLabel();
-JFrame frame = new JFrame();
-JPanel panel = new JPanel();
-public static void main(String[] args) {
-SlotMachine slot = new SlotMachine();
-slot.drawButtons();
-}
-public void drawButtons() {
-frame.add(panel);
+public class SlotMachine implements ActionListener {
+	Random rand = new Random();
+	JButton button = new JButton();
+	JLabel left = new JLabel();
+	JLabel center = new JLabel();
+	JLabel right = new JLabel();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	int score = 0;
+	public static void main(String[] args) {
+		SlotMachine slot = new SlotMachine();
+		slot.globglogabgalab1();
+		slot.globglogabgalab();	
+	}
+	public void globglogabgalab1() {
+		button.addActionListener(this);		
+	}
+	public void globglogabgalab() {
+		panel = new JPanel();
+		frame.add(panel);
 
-//Globglogabgalab=loadImage("globglogabgalab.png");
-///Book=loadImage("book.png");
-//Treasure=loadImage("treasure.png");
-/*
-panel.add(Globglogabgalab);
-panel.add(Book);
-panel.add(Treasure);
-*/
-frame.setVisible(true);
-panel.setVisible(true);
-int bob = rand.nextInt(3);
-System.out.println(bob);
-if (bob==0) {
-left=loadImage("globglogabgalab.png");
-}
-if (bob==1) {
-left=loadImage("book.png");
-}
-if (bob==2) {
-left=loadImage("treasure.png");
-}
-bob = rand.nextInt(3);
-if (bob==0) {
-center=loadImage("globglogabgalab.png");
-}
-if (bob==1) {
-center=loadImage("book.png");
-}
-if (bob==2) {
-center=loadImage("treasure.png");
-}
-bob = rand.nextInt(3);
-if (bob==0) {
-right=loadImage("globglogabgalab.png");
-}
-if (bob==1) {
-right=loadImage("book.png");
-}
-if (bob==2) {
-right=loadImage("treasure.png");
-}
-panel.add(left);
-panel.add(right);
-panel.add(center);
-frame.pack();
-}
-public JLabel loadImage(String fileName) {
-	URL imageURL = getClass().getResource(fileName);
-	Icon icon = new ImageIcon(imageURL);
-	return new JLabel(icon);
-}
+		frame.setVisible(true);
+		panel.setVisible(true);
+		int bob = rand.nextInt(3);
+		System.out.println(bob);
+		if (bob == 0) {
+			left = loadImage("globglogabgalab.png");
+		}
+		if (bob == 1) {
+			left = loadImage("book.png");
+		}
+		if (bob == 2) {
+			left = loadImage("treasure.png");
+		}
+		bob = rand.nextInt(3);
+		if (bob == 0) {
+			center = loadImage("globglogabgalab.png");
+		}
+		if (bob == 1) {
+			center = loadImage("book.png");
+		}
+		if (bob == 2) {
+			center = loadImage("treasure.png");
+		}
+		bob = rand.nextInt(3);
+		if (bob == 0) {
+			right = loadImage("globglogabgalab.png");
+		}
+		if (bob == 1) {
+			right = loadImage("book.png");
+		}
+		if (bob == 2) {
+			right = loadImage("treasure.png");
+		}
+		panel.add(left);
+		panel.add(right);
+		panel.add(center);
+		panel.add(button);
+		button.setText("Click Me To " + "Play Again!");
+		button.setVisible(true);
+		if (left == loadImage("globglogabgalab.png")) {
+			if (center == loadImage("globglogabgalab.png")) {
+				if (right == loadImage("globglogabgalab.png")) {
+					score += 1;
+					JOptionPane.showMessageDialog(null, "You have won " + score + " times");
+				}
+			}
+		}
+		if (left == loadImage("book.png")) {
+			if (center == loadImage("book.png")) {
+				if (right == loadImage("book.png")) {
+					score += 1;
+					JOptionPane.showMessageDialog(null, "You have won " + score + " times");
+				}
+			}
+		}
+		if (left == loadImage("treasure.png")) {
+			if (center == loadImage("treasure.png")) {
+				if (right == loadImage("treasure.png")) {
+					score += 1;
+					JOptionPane.showMessageDialog(null, "You have won " + score + " times");
+				}
+			}
+		}
+		left.setPreferredSize(new Dimension(500,500));
+		right.setPreferredSize(new Dimension(500,500));
+		center.setPreferredSize(new Dimension(500,500));
+		frame.pack();
+	}
+	public JLabel loadImage(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		return new JLabel(icon);
+	}
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource().equals(button)) {
+			frame.dispose();
+			frame = new JFrame();
+			left = new JLabel();
+			right = new JLabel();
+			center = new JLabel();
+			panel = new JPanel();
+			
+			globglogabgalab();
+		}
+	}
 }
--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -3,137 +3,123 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
 import java.util.Random;
-
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
 public class WhackAMole implements ActionListener {
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	Date d;
-	int score = 0;
-		static int joe;
-		int miss = 0;
-
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+Date d;
+int score = 0;
+static int joe;
+int miss = 0;
 int b;
-	Random rand = new Random();
-	JButton NoThisIsPatrick = new JButton("mole!");
-
-	public static void main(String[] args) {
-		WhackAMole whack = new WhackAMole();
-		whack.globglogabgalab();
-		String jeff = JOptionPane.showInputDialog("How many buttons do you want to create?");
-		joe = Integer.parseInt(jeff);
-		whack.drawButtons(joe);
-	}
-
-	public void globglogabgalab() {
-		frame.setSize(2040, 1900);
-	}
-
-	public void drawButtons(int bob) {
-		frame = new JFrame();
-		panel=new JPanel();
-		d = new Date();
-		b = rand.nextInt(bob);
-		for (int i = 0; i < bob; i++) {
-			System.out.println(i);
-			NoThisIsPatrick = new JButton();
-			NoThisIsPatrick.addActionListener(this);
-			panel.add(NoThisIsPatrick);
-			if (i == b) {
-				System.out.println("f" + b);
-				NoThisIsPatrick.setText("mole!");
-			}
-		}
-		frame.add(panel);
-		frame.setVisible(true);
-		frame.pack();
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		JButton buttonPressed = (JButton) event.getSource();
-		System.out.println(buttonPressed.getText());
-		if (buttonPressed.getText().equals("")) {
-			miss+=1;
-			if (miss==1) {
-				speak("I am the Glob-glo-gab-galab");
-			}
-			if (miss==2) {
-				speak("I love books, and this basement, is a true treasure trove");
-			}
-			if (miss==3) {
-				speak("I am the Glob-glo-gab-galab\n" + 
-						"The shwabble-dabble-wabble-gabble flibba blabba blab");
-			}
-			if (miss==4) {
-				speak("I'm full of shwibbly glib-a-kind\n" + 
-						"I am the yeast of thoughts and minds");
-			}
-			if (miss==5) {
-				speak("Shwabble dabble glibble glabble schribble shwap glab,\n" + 
-						"Dibble dabble shribble shrabble glibbi-glap shwap");
-			}
-			if (miss==6) {
-				speak("Shwabble dabble glibble glabble shwibble shwap-dap,\n" +  
-					"Dibble dabble shribble shrabble glibbi-shwap glab");
-			}
-			if (miss==7) {
-				speak("Oooh, ha ha ha, mmm, splendid\n" + 
-						"Simply delicious\n" + 
-						"Ooooohm, ha haa ha ha");
-			}
-			if (miss==8) {
-				speak("I am the Glob-glo-gab-galab\n" + 
-						"The shwabble-dabble-wabble-gabble flibba blabba blab");
-			}
-			if (miss==9) {
-				speak("I'm full of shwibbly glib-a-kind\n" + 
-						"I am the yeast of thoughts and minds");
-			}
-			if (miss==10) {
-				speak("Shwabble dabble glibble glabble schribble shwap glab\n" + 
-						"Dibble dabble shribble shrabble glibbi-glap shwap\n" + 
-						"Shwabble dabble glibble glabble shwibble shwap-dap\n" + 
-						"Dibble dabble shribble shrabble glibbi-shwap glab");
-				speak("You are a looooooooser. please try again.");
-			}
-		}
-		if (buttonPressed.getText().equals("mole!")) {
-			score += 1;
-			playSound("1.wav");
-			
-		}
-		if (score == 10) {
-			endGame(d, 10);
-		}
-		frame.dispose();
-		globglogabgalab();
-		drawButtons(joe);
-		
-	}
-
-	void speak(String words) {
-		try {
-			Runtime.getRuntime().exec("say " + words).waitFor();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void endGame(Date timeAtStart, int molesWhacked) {
-		Date timeAtEnd = new Date();
-		JOptionPane.showMessageDialog(null, "Your whack rate is "
-				+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
-	}
-	private void playSound(String fileName) {
-	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
-	     sound.play();
-	}
-
+Random rand = new Random();
+JButton NoThisIsPatrick = new JButton("mole!");
+public static void main(String[] args) {
+WhackAMole whack = new WhackAMole();
+whack.globglogabgalab();
+String jeff = JOptionPane.showInputDialog("How many buttons do you want to create?");
+joe = Integer.parseInt(jeff);
+whack.drawButtons(joe);
+}
+public void globglogabgalab() {
+frame.setSize(2040, 1900);
+}
+public void drawButtons(int bob) {
+frame = new JFrame();
+panel=new JPanel();
+d = new Date();
+b = rand.nextInt(bob);
+for (int i = 0; i < bob; i++) {
+System.out.println(i);
+NoThisIsPatrick = new JButton();
+NoThisIsPatrick.addActionListener(this);
+panel.add(NoThisIsPatrick);
+if (i == b) {
+System.out.println("f" + b);
+NoThisIsPatrick.setText("mole!");
+}
+}
+frame.add(panel);
+frame.setVisible(true);
+frame.pack();
+}
+public void actionPerformed(ActionEvent event) {
+JButton buttonPressed = (JButton) event.getSource();
+System.out.println(buttonPressed.getText());
+if (buttonPressed.getText().equals("")) {
+miss+=1;
+if (miss==1) {
+speak("I am the Glob-glo-gab-galab");
+}
+if (miss==2) {
+speak("I love books, and this basement, is a true treasure trove");
+}
+if (miss==3) {
+speak("I am the Glob-glo-gab-galab\n" + 
+"The shwabble-dabble-wabble-gabble flibba blabba blab");
+}
+if (miss==4) {
+speak("I'm full of shwibbly glib-a-kind\n" + 
+"I am the yeast of thoughts and minds");
+}
+if (miss==5) {
+speak("Shwabble dabble glibble glabble schribble shwap glab,\n" + 
+"Dibble dabble shribble shrabble glibbi-glap shwap");
+}
+if (miss==6) {
+speak("Shwabble dabble glibble glabble shwibble shwap-dap,\n" +  
+"Dibble dabble shribble shrabble glibbi-shwap glab");
+}
+if (miss==7) {
+speak("Oooh, ha ha ha, mmm, splendid\n" + 
+"Simply delicious\n" + 
+"Ooooohm, ha haa ha ha");
+}
+if (miss==8) {
+speak("I am the Glob-glo-gab-galab\n" + 
+"The shwabble-dabble-wabble-gabble flibba blabba blab");
+}
+if (miss==9) {
+speak("I'm full of shwibbly glib-a-kind\n" + 
+"I am the yeast of thoughts and minds");
+}
+if (miss==10) {
+speak("Shwabble dabble glibble glabble schribble shwap glab\n" + 
+"Dibble dabble shribble shrabble glibbi-glap shwap\n" + 
+"Shwabble dabble glibble glabble shwibble shwap-dap\n" + 
+"Dibble dabble shribble shrabble glibbi-shwap glab");
+speak("You are a looooooooser. please try again.");
+}
+}
+if (buttonPressed.getText().equals("mole!")) {
+score += 1;
+playSound("1.wav");		
+}
+if (score == 10) {
+endGame(d, 10);
+}
+frame.dispose();
+globglogabgalab();
+drawButtons(joe);
+}
+void speak(String words) {
+try {
+Runtime.getRuntime().exec("say " + words).waitFor();
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+private void endGame(Date timeAtStart, int molesWhacked) {
+Date timeAtEnd = new Date();
+JOptionPane.showMessageDialog(null, "Your whack rate is "
++ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+}
+private void playSound(String fileName) {
+AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+sound.play();
+}
 }
