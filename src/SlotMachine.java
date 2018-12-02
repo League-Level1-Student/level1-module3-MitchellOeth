b6bf88f022446c9ad4ecec2ebb24cf6b6a9e97e4
League <League@192.168.7.204>
2018-12-02 11:34:26 -0800

    slot

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -10,6 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
 public class SlotMachine implements ActionListener {
 	Random rand = new Random();
 	JButton button = new JButton();
@@ -19,14 +20,17 @@ public class SlotMachine implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	int score = 0;
+
 	public static void main(String[] args) {
 		SlotMachine slot = new SlotMachine();
 		slot.globglogabgalab1();
-		slot.globglogabgalab();	
+		slot.globglogabgalab();
 	}
+
 	public void globglogabgalab1() {
-		button.addActionListener(this);		
+		button.addActionListener(this);
 	}
+
 	public void globglogabgalab() {
 		panel = new JPanel();
 		frame.add(panel);
@@ -44,24 +48,24 @@ public class SlotMachine implements ActionListener {
 		if (bob == 2) {
 			left = loadImage("treasure.png");
 		}
-		bob = rand.nextInt(3);
-		if (bob == 0) {
+		int joe = rand.nextInt(3);
+		if (joe == 0) {
 			center = loadImage("globglogabgalab.png");
 		}
-		if (bob == 1) {
+		if (joe == 1) {
 			center = loadImage("book.png");
 		}
-		if (bob == 2) {
+		if (joe == 2) {
 			center = loadImage("treasure.png");
 		}
-		bob = rand.nextInt(3);
-		if (bob == 0) {
+		int billeth = rand.nextInt(3);
+		if (billeth == 0) {
 			right = loadImage("globglogabgalab.png");
 		}
-		if (bob == 1) {
+		if (billeth == 1) {
 			right = loadImage("book.png");
 		}
-		if (bob == 2) {
+		if (billeth == 2) {
 			right = loadImage("treasure.png");
 		}
 		panel.add(left);
@@ -70,33 +74,44 @@ public class SlotMachine implements ActionListener {
 		panel.add(button);
 		button.setText("Click Me To " + "Play Again!");
 		button.setVisible(true);
-		if (left == loadImage("globglogabgalab.png")) {
-			if (center == loadImage("globglogabgalab.png")) {
-				if (right == loadImage("globglogabgalab.png")) {
+		
+		/*
+		if (left.equals(loadImage("globglogabgalab.png"))) {
+			if (center.equals(loadImage("globglogabgalab.png"))) {
+				if (right.equals(loadImage("globglogabgalab.png"))) {
 					score += 1;
 					JOptionPane.showMessageDialog(null, "You have won " + score + " times");
 				}
 			}
 		}
-		if (left == loadImage("book.png")) {
-			if (center == loadImage("book.png")) {
-				if (right == loadImage("book.png")) {
+		if (left.equals(loadImage("book.png"))) {
+			if (center.equals(loadImage("book.png"))) {
+				if (right.equals(loadImage("book.png"))) {
 					score += 1;
 					JOptionPane.showMessageDialog(null, "You have won " + score + " times");
 				}
 			}
 		}
-		if (left == loadImage("treasure.png")) {
-			if (center == loadImage("treasure.png")) {
-				if (right == loadImage("treasure.png")) {
+		if (left.equals(loadImage("treasure.png"))) {
+			if (center.equals(loadImage("treasure.png"))) {
+				if (right.equals(loadImage("treasure.png"))) {
 					score += 1;
 					JOptionPane.showMessageDialog(null, "You have won " + score + " times");
 				}
 			}
 		}
-		left.setPreferredSize(new Dimension(500,500));
-		right.setPreferredSize(new Dimension(500,500));
-		center.setPreferredSize(new Dimension(500,500));
+		*/
+		left.setPreferredSize(new Dimension(570, 1000));
+		right.setPreferredSize(new Dimension(570, 1000));
+		center.setPreferredSize(new Dimension(570, 1000));
+		if(bob==billeth) {
+			if (bob==joe) {
+				if(billeth==joe) {
+					score+=1;
+					JOptionPane.showMessageDialog(null, "You have won " + score + " times");
+				}
+			}
+		}
 		frame.pack();
 	}
 	public JLabel loadImage(String fileName) {
@@ -112,7 +127,6 @@ public class SlotMachine implements ActionListener {
 			right = new JLabel();
 			center = new JLabel();
 			panel = new JPanel();
-			
 			globglogabgalab();
 		}
 	}
